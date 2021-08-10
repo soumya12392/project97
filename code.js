efe1f279df5e6fd264be2a3b264156daf5f0ab68
Soumya Singh <83289414+soumya12392@users.noreply.github.com>
2021-08-10 19:43:39 +0530

    Add files via upload
    Completed project 97!

---
+++ code.js
@@ -0,0 +1,39 @@
+function o1() {
+    document.getElementById("image").src = "O-.png";
+    console.log("Blood group = O-");
+}
+
+function o2() {
+    document.getElementById("image").src = "O+.png";
+    console.log("Blood group = O+");
+}
+
+function a1() {
+    document.getElementById("image").src = "A-.png";
+    console.log("Blood group = A-");
+}
+
+function a2() {
+    document.getElementById("image").src = "A+.png";
+    console.log("Blood group = A+");
+}
+
+function b1() {
+    document.getElementById("image").src = "B-.png";
+    console.log("Blood group = B-");
+}
+
+function b2() {
+    document.getElementById("image").src = "B+.png";
+    console.log("Blood group = B+");
+}
+
+function ab1() {
+    document.getElementById("image").src = "AB-.png";
+    console.log("Blood group = AB-");
+}
+
+function ab2() {
+    document.getElementById("image").src = "AB+.png";
+    console.log("Blood group = AB+");
+}
